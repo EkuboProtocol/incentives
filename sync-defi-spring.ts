c87409efe7483fc42360bd2782e69f5338905483
Moody Salem <moody.salem@gmail.com>
2025-05-15 23:48:47 +0100

    copy discretionary handling from the api

--- sync-defi-spring.ts
+++ sync-defi-spring.ts
@@ -7,6 +7,22 @@ const incentiveDataResponse = await fetch(
     "https://kx58j6x5me.execute-api.us-east-1.amazonaws.com/starknet/fetchFile?file=strk_grant.json",
 );
 
+// generic per-exchange mapping of pair ⇒ data[]
+type ExchangeData = Record<`${string}/${string}`, PoolData[]>;
+
+interface OBLApiResponse {
+  Ekubo: ExchangeData;
+  MySwap: ExchangeData;
+  Haiko: ExchangeData;
+  "10kSwap": ExchangeData;
+  StarkDefi: ExchangeData;
+  Sithswap: ExchangeData;
+  Nostra: ExchangeData;
+  Jediswap_v1: ExchangeData;
+  Jediswap_v2: ExchangeData;
+  Haiko_Solvers: ExchangeData;
+}
+
 if (!incentiveDataResponse.ok) {
   throw new Error(
     `Failed to fetch incentive data: ${await incentiveDataResponse.text()}`,
@@ -22,6 +38,133 @@ if (!tokensResponse.ok) {
     `Failed to fetch tokens data: ${await tokensResponse.text()}`,
   );
 }
+
+function addIncentivesToData(
+  map: OBLApiResponse["Ekubo"],
+  { pairId, date, amount }: { pairId: string; date: string; amount: number },
+) {
+  const [tokenA, tokenB] = pairId.split("/");
+  const pairDailyData =
+    map[`${tokenA}/${tokenB}`] ?? map[`${tokenB}/${tokenA}`];
+
+  if (!pairDailyData) {
+    map[`${tokenA}/${tokenB}`] = [
+      {
+        date,
+        allocation: amount,
+        // we use the value 1 as a fallback
+        thirty_day_realized_volatility: 1,
+        token0_allocation: amount / 2,
+        token1_allocation: amount / 2,
+      },
+    ];
+  } else {
+    const dayData = pairDailyData.find((d) => d.date === date);
+    if (dayData) {
+      dayData.allocation += amount;
+      dayData.token0_allocation += amount / 2;
+      dayData.token1_allocation += amount / 2;
+    } else {
+      pairDailyData.push({
+        date,
+        allocation: amount,
+        thirty_day_realized_volatility: 1,
+        token0_allocation: amount / 2,
+        token1_allocation: amount / 2,
+      });
+    }
+  }
+}
+
+const SPLITS_BY_DATE_RANGE = [
+  {
+    start: new Date("2024-03-21"),
+    end: new Date("2024-03-30"),
+    splits: [
+      {
+        pairId: "ZEND/ETH",
+        weight: 1,
+      },
+      {
+        pairId: "LORDS/ETH",
+        weight: 10,
+      },
+      {
+        pairId: "rETH/ETH",
+        weight: 1,
+      },
+      {
+        pairId: "ETH/USDT",
+        weight: 3,
+      },
+    ],
+  },
+  {
+    start: new Date("2024-03-30"),
+    end: new Date("2024-03-31"),
+    splits: [
+      {
+        pairId: "ZEND/ETH",
+        weight: 1,
+      },
+      {
+        pairId: "LORDS/ETH",
+        weight: 20,
+      },
+      {
+        pairId: "rETH/ETH",
+        weight: 3,
+      },
+      {
+        pairId: "ETH/USDT",
+        weight: 9,
+      },
+    ],
+  },
+  {
+    start: new Date("2024-03-31"),
+    end: new Date("2024-04-04"),
+    splits: [
+      {
+        pairId: "ZEND/ETH",
+        weight: 1,
+      },
+      {
+        pairId: "LORDS/ETH",
+        weight: 20,
+      },
+      {
+        pairId: "rETH/ETH",
+        weight: 3,
+      },
+      {
+        pairId: "ETH/USDT",
+        weight: 12,
+      },
+    ],
+  },
+  {
+    start: new Date("2024-04-04"),
+    splits: [
+      {
+        pairId: "ETH/USDC",
+        weight: 4,
+      },
+      {
+        pairId: "STRK/ETH",
+        weight: 12,
+      },
+      {
+        pairId: "STRK/USDC",
+        weight: 4,
+      },
+      {
+        pairId: "WBTC/ETH",
+        weight: 2,
+      },
+    ],
+  },
+];
 
 interface Token {
   name: string;
@@ -42,34 +185,41 @@ interface PoolData {
   token0_allocation: number;
   token1_allocation: number;
   thirty_day_realized_volatility: number;
-  tvl_usd: number;
-  apr: number;
 }
 
-// generic per-exchange mapping of pair ⇒ data[]
-type ExchangeData = Record<`${string}/${string}`, PoolData[]>;
+const incentiveData = (await incentiveDataResponse.json()) as OBLApiResponse;
 
-interface ApiResponse {
-  Ekubo: ExchangeData;
-  MySwap: ExchangeData;
-  Haiko: ExchangeData;
-  "10kSwap": ExchangeData;
-  StarkDefi: ExchangeData;
-  Sithswap: ExchangeData;
-  Nostra: ExchangeData;
-  Jediswap_v1: ExchangeData;
-  Jediswap_v2: ExchangeData;
-  Haiko_Solvers: ExchangeData;
+{
+  // manipulate the response object, replacing discretionary with our own allocations
+  const discretionary = incentiveData.Ekubo["Discretionary"];
+  delete incentiveData.Ekubo["Discretionary"];
+
+  if (discretionary) {
+    discretionary.forEach(({ date, allocation }) => {
+      const d = new Date(date);
+      const matching = SPLITS_BY_DATE_RANGE.find(
+        (s) => d >= s.start && (!s.end || d < s.end),
+      );
+
+      if (!matching) return;
+
+      const totalWeight = matching.splits.reduce((m, v) => m + v.weight, 0);
+
+      matching.splits.forEach(({ pairId, weight }) => {
+        addIncentivesToData(incentiveData.Ekubo, {
+          pairId,
+          date,
+          amount: Math.floor((allocation * weight * 1000) / totalWeight) / 1000,
+        });
+      });
+    });
+  }
 }
-
-const incentiveData = (await incentiveDataResponse.json()) as ApiResponse;
 
 const ekuboIncentivesData = incentiveData.Ekubo;
 
 const incentiveRwardPeriodRowData = Object.entries(ekuboIncentivesData).flatMap(
   ([pair, data]) => {
-    if (pair === "Discretionary") return [];
-
     const [symbolA, symbolB] = pair.split("/");
 
     const tokenA = tokensJson.find((t) => t.symbol.toUpperCase() === symbolA);
